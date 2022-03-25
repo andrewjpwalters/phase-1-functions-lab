1a4f42de9d05c96656162d222f561c1afa6f9f1c
Andrew Walters <andrewjpwalters@gmail.com>
2022-03-25 00:58:27 -0500

    commit tree b7bfb8dd7a41df959e3ed8645a4b6806cfe46bcd

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    return Math.abs(42 - block)
+};
+
+function distanceFromHqInFeet(block) {
+    const blocks = distanceFromHqInBlocks(block);
+    return blocks * 264;
+};
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs(start - end) * 264;
+};
+
+function calculatesFarePrice(start, end) {
+    const feet = distanceTravelledInFeet(start, end);
+    if (feet < 400) {
+        return 0
+    }
+    else if (feet > 2500) {
+        return "cannot travel that far"
+    }
+    else if (feet > 2000) {
+        return 25
+    } else {
+        return (feet - 400) * 0.02
+    }
+}
